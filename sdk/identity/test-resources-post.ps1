ffbee11bbe9158f7e84049236131e66ead8a76dd
Vinay Gera <vigera@microsoft.com>
2024-02-26 10:18:53 -0800

    cleanup script

--- sdk/identity/test-resources-post.ps1
+++ sdk/identity/test-resources-post.ps1
@@ -27,13 +27,9 @@ Write-Host "webappRootPom: $webappRootPom"
 $funcAppRoot = "$PSScriptRoot/live-test-apps/identity-test-function" | Resolve-Path
 $funcAppPom = "$funcAppRoot/pom.xml" | Resolve-Path
 
-# if ($null -ne $Env:AGENT_WORKFOLDER) {
-#     $workingFolder = $Env:AGENT_WORKFOLDER
-# }
 az login --service-principal -u $(getVariable('IDENTITY_CLIENT_ID')) -p $(getVariable('IDENTITY_CLIENT_SECRET')) --tenant $(getVariable('IDENTITY_TENANT_ID'))
 az account set --subscription $(getVariable('IDENTITY_SUBSCRIPTION_ID'))
 
-mvn --version | Write-Host
 
 mvn clean install -DskipTests -f $webappRootPom | Write-Host
 az webapp deploy --resource-group $(getVariable('IDENTITY_RESOURCE_GROUP')) --name $(getVariable('IDENTITY_WEBAPP_NAME')) --src-path "$webappRoot/target/identity-test-webapp-0.0.1-SNAPSHOT.jar" --type jar
@@ -42,24 +38,9 @@ az webapp deploy --resource-group $(getVariable('IDENTITY_RESOURCE_GROUP')) --na
 #     Remove-Item -Force -Recurse "$webappRoot/%AGENT_WORKFOLDER%"
 # }
 
-if (Test-Path -Path "$webappRoot/target/identity-test-webapp-0.0.1-SNAPSHOT.jar" -PathType Leaf) {
-    Write-Host "The file exists!"
-} else {
-    Write-Host "The file does not exist."
-}
-
-Write-Host "working on function"
-
-pip install
 
 # build function app
 mvn clean package "-DfunctionAppName=$(getVariable('IDENTITY_FUNCTION_NAME'))" "-DresourceGroup=$(getVariable('IDENTITY_RESOURCE_GROUP'))" "-DappServicePlanName=$(getVariable('IDENTITY_APPSERVICE_NAME'))" -f $funcAppPom | Write-Host
 compress-archive  "$funcAppRoot\target\azure-functions\$(getVariable('IDENTITY_FUNCTION_NAME'))\*" -DestinationPath "$funcAppRoot/target/funcpackage.zip"
-
-if (Test-Path -Path "$funcAppRoot\target\funcpackage.zip" -PathType Leaf) {
-    Write-Host "The func zip file exists!"
-} else {
-    Write-Host "The func  zip file does not exist."
-}
 az functionapp deployment source config-zip -g $(getVariable('IDENTITY_RESOURCE_GROUP')) -n $(getVariable('IDENTITY_FUNCTION_NAME')) --src "$funcAppRoot/target/funcpackage.zip"
 az logout
